14a7c53e7b8da8605911086435d6447e0e3f620b
nailKhaled <khaaled792@gmail.com>
2021-08-20 22:09:55 +0200

    School-Management-Demo-JavaFx

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,32 @@
+package controller;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void closeAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void loginAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
