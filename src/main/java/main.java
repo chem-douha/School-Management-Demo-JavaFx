cf7bfe964fd69bfcc29adb92ff725ae6a05e8b02
nailKhaled <khaaled792@gmail.com>
2021-08-20 22:09:38 +0200

    School-Management-Demo-JavaFx

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,11 +3,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class main extends Application {
     public void start(Stage primaryStage) throws Exception {
-
+        Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("تسجيل الدخول");
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.show();
 
 
     }
